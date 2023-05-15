eee1ea17c1d708f2d66cc9ec2b2cc16ff3f4c177
vivek-mahajan-xero <vivek.mahajan@xero.com>
2023-05-15 23:23:06 +1000

    Add dynamo db queries

--- Sample.WebApi/Controllers/WeatherForecastController.cs
+++ Sample.WebApi/Controllers/WeatherForecastController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -32,8 +33,31 @@ namespace Sample.WebApi.Controllers
         public async Task<IEnumerable<WeatherForecast>> Get(string city = "Melbourne")
         {
             return await _dynamoDbContext
-                .QueryAsync<WeatherForecast>(city)
+                .QueryAsync<WeatherForecast>(city, QueryOperator.Between,
+                    new object[] { DateTime.Now.AddDays(-30), DateTime.Now })
                 .GetRemainingAsync();
+        }
+
+        [HttpPost]
+        public async Task Post(string city)
+        {
+            // var data = GenerateDummyWeatherForecast(city);
+            // foreach (var item in data)
+            // {
+            //     await _dynamoDbContext.SaveAsync(item);
+            // }
+
+            var specificItem = await _dynamoDbContext.LoadAsync<WeatherForecast>(city, DateTime.Now.Date.AddDays(1));
+            specificItem.Summary = "Warm";
+            await _dynamoDbContext.SaveAsync(specificItem);
+        }
+
+        [HttpDelete]
+        public async Task Delete(string city)
+        {
+            var specificItem =await _dynamoDbContext.LoadAsync<WeatherForecast>(city, DateTime.Now.Date.AddDays(1));
+            // await _dynamoDbContext.DeleteAsync<WeatherForecast>(city, DateTime.Now.Date.AddDays(1));
+            await _dynamoDbContext.DeleteAsync(specificItem);
         }
 
         private static IEnumerable<WeatherForecast> GenerateDummyWeatherForecast(string city)
@@ -42,7 +66,7 @@ namespace Sample.WebApi.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
                     City = city,
-                    Date = DateTime.Now.AddDays(index),
+                    Date = DateTime.Now.Date.AddDays(index),
                     TemperatureC = rng.Next(-20, 55),
                     Summary = Summaries[rng.Next(Summaries.Length)]
                 })
--- Sample.WebApi/Startup.cs
+++ Sample.WebApi/Startup.cs
@@ -1,10 +1,13 @@
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Runtime;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Sample.WebApi.Registrations;
 
 namespace Sample.WebApi
 {
@@ -25,7 +28,16 @@ namespace Sample.WebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sample.WebApi", Version = "v1" });
             });
-            services.AddAwsServices(Configuration);
+            // services.AddAwsServices(Configuration);
+            var credentials = new BasicAWSCredentials("", "");
+            var config = new AmazonDynamoDBConfig
+            {
+                RegionEndpoint = RegionEndpoint.USEast1,
+                ServiceURL = "http://127.0.0.1:4566"
+            };
+            var client = new AmazonDynamoDBClient(credentials, config);
+            services.AddSingleton<IAmazonDynamoDB>(client);
+            services.AddSingleton<IDynamoDBContext, DynamoDBContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
